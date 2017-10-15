4ef01424d844c94314c96399861a179dfe2e64a7
c457 <android.c357@gmail.com>
2017-10-15 05:06:20 -0400

    molly: Populate BoardConfig Makefile
    * These values were all obtained through dumping the devices stock boot image and analyzing the values.
    * Remove flags that don't pertain to us.
    * Alphabetize (excluding vendor includes and overlays which must come first so that they can override what is to follow).

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,36 +11,29 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License
+# limitations under the License.
 
-USE_CAMERA_STUB := true
-
-# inherit from the proprietary version
--include vendor/__MANUFACTURER__/__DEVICE__/BoardConfigVendor.mk
-
+# Architecture
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_VARIANT := cortex-a15
 
-TARGET_BOOTLOADER_BOARD_NAME := __DEVICE__
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := molly
+TARGET_BOARD_PLATFORM := tegra4
+TARGET_NO_BOOTLOADER := true
 
-BOARD_KERNEL_CMDLINE := __CMDLINE__
-BOARD_KERNEL_BASE := 0x__BASE__
-BOARD_KERNEL_PAGESIZE := __PAGE_SIZE__
+# Camera
+USE_CAMERA_STUB := true
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
+# Kernel
+BOARD_KERNEL_CMDLINE :=  androidboot.hardware=molly console=ttyFIQ0 smsc95xx.boot_wol_config=0x07 smsc95xx.turbo_mode=N
 
-TARGET_PREBUILT_KERNEL := device/__MANUFACTURER__/__DEVICE__/kernel
-
-BOARD_HAS_NO_SELECT_BUTTON := true
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13912506368
+BOARD_FLASH_BLOCK_SIZE := 4096
