710b8596944199025b87fbb720d600b8474a30c2
Nolen Johnson <johnsonnolen@gmail.com>
2017-11-17 18:18:25 -0500

    molly: Build kernel inline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,8 +43,10 @@ SF_VSYNC_EVENT_PHASE_OFFSET_NS := 1
 
 # Kernel
 BOARD_KERNEL_CMDLINE :=  androidboot.hardware=molly console=ttyFIQ0 smsc95xx.boot_wol_config=0x07 smsc95xx.turbo_mode=N
+TARGET_KERNEL_SOURCE := kernel/google/molly
+TARGET_KERNEL_CONFIG := molly_defconfig
 
-# Partitions
+# Partition
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
